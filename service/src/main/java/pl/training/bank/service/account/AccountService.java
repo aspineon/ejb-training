62880c7d92ada823891ef26996d5a74b3c78fe4e
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-05-15 09:52:00 +0200

    Step 3

--- service/src/main/java/pl/training/bank/service/account/AccountService.java
+++ service/src/main/java/pl/training/bank/service/account/AccountService.java
@@ -1,14 +1,15 @@
 package pl.training.bank.service.account;
 
 import lombok.Setter;
-import pl.training.bank.entity.account.Account;
 import pl.training.bank.account.InsufficientFundsException;
+import pl.training.bank.entity.account.Account;
+import pl.training.bank.service.operation.DepositLimitInterceptor;
 
 import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.interceptor.Interceptors;
 import javax.jms.ConnectionFactory;
-import javax.jms.JMSContext;
 import javax.jms.Topic;
 
 @Setter
@@ -19,10 +20,6 @@ public class AccountService {
     private AccountNumberGenerator accountNumberGenerator;
     @EJB
     private AccountRepository accountRepository;
-    @Resource(lookup = "java:jboss/exported/jms/RemoteConnectionFactory")
-    private ConnectionFactory connectionFactory;
-    @Resource(lookup = "java:jboss/exported/jms/topic/Bank")
-    private Topic topic;
 
     public Account createAccount() {
         String accountNumber = accountNumberGenerator.getNext();
@@ -31,13 +28,8 @@ public class AccountService {
         return account;
     }
 
+    @Interceptors(DepositLimitInterceptor.class)
     public void deposit(long funds, String accountNumber) {
-        if (funds > 1000) {
-            String message = "Deposit limit on account: " + accountNumber;
-            try (JMSContext jmsContext = connectionFactory.createContext()) {
-                jmsContext.createProducer().send(topic, message);
-            }
-        }
         Account account = accountRepository.getByNumber(accountNumber);
         account.deposit(funds);
     }
